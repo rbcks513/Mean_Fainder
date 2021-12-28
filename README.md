072cac808880f851bd099727c103a37e333f45f7
rbcks513 <rbcks513@naver.com>
2021-12-28 22:06:47 +0900

    Delete csv files, Correction variables and form, Add annotation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Mean_Fainder
+
+<h1>functions to be perfect</h1>
+<p>
+1. "," in mean sentence will be removed in the sentence
+2. Show how many cycle remaining
+3. if input words have typo, program show that this word has typo and go next cycle
+</p>
--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import pandas as pd
 import csv 
 import datetime
 
@@ -8,6 +7,9 @@ def main():
     now = datetime.datetime.now()
     today = str(now.month) + str(now.day)
     f = open("voca.csv", "r")
+    
+    #this program make new file once a day 
+    #thus we do not have to check whether file exsit or not
     new = open(today+"mean.csv", "w")
     new = open(today+"mean.csv", "a")
     reader = csv.reader(f)
@@ -16,36 +18,39 @@ def main():
     for row in reader:
         temp = row[0]
         voca[n] = temp
+        
+        
         if ( n != 0): 
-            means = Find_in_dictionary(voca[n])
+            means = find_in_dictionary(voca[n])
             data = (row[0] + ", " + means+"\n")
             new.write(data)
         n = n + 1
         
-        
     f.close()
- 
+    new.close()
         
-
-def Find_in_dictionary(voca):
+def find_in_dictionary(voca):
+    #Naver dicitionary has two different url, one can be accessed, other cannot be accessed.
     dictionary_url = "https://dict.naver.com/search.dict?mode=all&query_euckr=&query=%s"%voca
     #dictionary_url = 'https://en.dict.naver.com/#/search?query=%s&range=all'%voca
     
-    asd = urllib.request.urlopen(dictionary_url)
+    requsted_url = urllib.request.urlopen(dictionary_url)
 
-    a = BeautifulSoup(asd, 'html.parser')
-    mean = a.find("dl", class_ = "dic_search_result")
+    bs_obj = BeautifulSoup(requsted_url, 'html.parser')
+    
+    #dl is html tag
+    dl = bs_obj.find("dl", class_ = "dic_search_result")
+    
+    #temp will contain factor in the list
     temp = ""
-    for mn in mean.find("dd"):
+    
+    #Select in tag 'dd'
+    for mn in dl.find("dd"):
         m = mn.string
         if(m == None): continue
         k = m.strip()
         temp = temp +" "+ k
-      
-        
     return temp
-        
-
 
 if __name__ == '__main__':
     main()
